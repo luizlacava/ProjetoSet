0be1c13c21aea7f74587870ad54f2e15bc7186c6
luizlacava <lglacava@gmail.com>
2024-06-19 10:13:26 -0300

    Atividade de agenda de contato atualizada
    Signed-off-by: luizlacava <lglacava@gmail.com>

---
+++ src/Pesquisa/AgendaContatos.java
@@ -0,0 +1,61 @@
+package Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    //atributo
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero){
+        contatoSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContato(){
+        System.out.println(contatoSet);
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome){
+        Set<Contato> contatosPorNome = new HashSet<>();
+        for(Contato c : contatoSet) {
+           if(c.getNome().startsWith(nome)){
+               contatosPorNome.add(c);
+           }
+        }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero){
+        Contato contatoAtualizado = null;
+        for(Contato c : contatoSet){
+            if(c.getNome().equalsIgnoreCase(nome)){
+                c.setNumero(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.exibirContato();
+
+        agendaContatos.adicionarContato("Luiz", 12345);
+        agendaContatos.adicionarContato("Luiz", 415613);
+        agendaContatos.adicionarContato("Luiz Lacava", 1321465);
+        agendaContatos.adicionarContato("Dani", 98496516);
+        agendaContatos.adicionarContato("Daniela", 6541981);
+
+        agendaContatos.exibirContato();
+
+        System.out.println(agendaContatos.pesquisarPorNome("Luiz"));
+
+        System.out.println("Contato Atualizado " + agendaContatos.atualizarNumeroContato("Dani", 251698));
+    }
+}
